086fc735c3a94d6dafef0f0512c6771da247b7fa
sami12344 <freelancersami16203@gmail.com>
2023-07-14 16:16:15 +0600

    Between and in

---
+++ BETWEEN Operator.sql
@@ -0,0 +1,5 @@
+-- SELECT * FROM PersonalWithConstraints
+-- WHERE age BETWEEN 18 AND 20 
+
+SELECT * FROM PersonalWithConstraints
+WHERE age NOT BETWEEN 18 AND 20 
---
+++ In operator.sql
@@ -0,0 +1,10 @@
+-- SELECT * FROM PersonalWithConstraints
+-- WHERE age = 18 OR age = 21
+
+
+-- SELECT * FROM PersonalWithConstraints
+-- WHERE age IN (18,21,19)
+
+
+SELECT * FROM PersonalWithConstraints
+WHERE age NOT IN (18,21)
