aecf0a42fa487311aa1e2072844f35d589d9efa7
sami12344 <freelancersami16203@gmail.com>
2023-07-14 23:03:07 +0600

    primary

--- Primary key.sql
+++ Primary key.sql
@@ -1,6 +1,11 @@
-CREATE TABLE city (
- cid INT NOT NULL AUTO_INCREMENT,
- cityname VARCHAR(50) NOT NULL,
- PRIMARY KEY (cid)
+-- CREATE TABLE city (
+--  cid INT NOT NULL AUTO_INCREMENT,
+--  cityname VARCHAR(50) NOT NULL,
+--  PRIMARY KEY (cid)
 
-)
+-- )
+
+-- INSERT INTO city (cityname)
+-- VALUES ('Agra'), ('Delhi'),('Bhopal'),('jaipur'),('noida')
+
+SELECT * FROM city
