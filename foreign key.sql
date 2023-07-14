6da6b3655005a4eaf1d180bc10eec977d505a3a4
sami12344 <freelancersami16203@gmail.com>
2023-07-14 23:19:22 +0600

    foreign key

---
+++ foreign key.sql
@@ -0,0 +1,36 @@
+-- CREATE Table persons(
+--  id INT NOT NULL,
+--  name VARCHAR(50) NOT NULL,
+--  percentage INT NOT NULL,
+--  age INT NOT NULL,
+--  gender VARCHAR(1) NOT NULL,
+--  city INT NOT NULL,
+--  PRIMARY KEY (id),
+--  FOREIGN KEY (city) REFERENCES City (cid)
+-- )
+
+-- INSERT INTO Persons (id,name,percentage,age,gender,city)
+-- VALUES
+--    (1, 'John Doe', 80, 25, 'M', 1),
+--   (2, 'Jane Smith', 92, 32, 'F', 2),
+--   (3, 'Michael Johnson', 75, 40, 'M', 1),
+--   (4, 'Emily Davis', 88, 28, 'F', 3),
+--   (5, 'David Brown', 79, 35, 'M', 2),
+--   (6, 'Sarah Wilson', 95, 29, 'F', 3),
+--   (7, 'Daniel Lee', 83, 38, 'M', 1),
+--   (8, 'Olivia Martinez', 90, 27, 'F', 2),
+--   (9, 'Matthew Thompson', 85, 33, 'M', 3),
+--   (10, 'Emma Anderson', 91, 31, 'F', 1),
+--   (11, 'Christopher Taylor', 78, 26, 'M', 2),
+--   (12, 'Sophia Walker', 89, 34, 'F', 3),
+--   (13, 'William Clark', 87, 30, 'M', 1),
+--   (14, 'Ava Rodriguez', 82, 37, 'F', 2),
+--   (15, 'James Harris', 93, 24, 'M', 3),
+--   (16, 'Mia Young', 76, 39, 'F', 1),
+--   (17, 'Benjamin Turner', 84, 36, 'M', 2),
+--   (18, 'Charlotte Moore', 94, 23, 'F', 3),
+--   (19, 'Alexander Scott', 77, 41, 'M', 1),
+--   (20, 'Lily Baker', 86, 22, 'F', 2);
+
+
+SELECT * FROM persons
