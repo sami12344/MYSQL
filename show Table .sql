05b1c21a15be8f873d11376ad7ca6e08716bd20a
sami12344 <freelancersami16203@gmail.com>
2023-07-14 15:51:36 +0600

    update

--- show Table .sql
+++ show Table .sql
@@ -1,3 +1,3 @@
 -- SELECT * FROM personal
 -- SELECT id ,name,phone FROM personal
-SELECT id AS Id,name AS Student, phone AS Phone FROM personal
+SELECT id AS Id,name AS "Student name", phone AS Phone FROM personal
